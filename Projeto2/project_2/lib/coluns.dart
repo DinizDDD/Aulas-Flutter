153387e783f5701cd49f5a4854b51c1cb7de35b2
DadiAugusto <davidinizdalevedo9@gmail.com>
2025-04-11 08:35:33 -0300

    more things to learn

---
+++ Projeto2/project_2/lib/coluns.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Colunas());
+}
+
+class Colunas extends StatelessWidget {
+  const Colunas({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder(
+     
+    );
+  }
+}
