0fafa6681e621ac5141c7dce2022246d00ccea24
DadiAugusto <davidinizdalevedo9@gmail.com>
2025-04-25 09:36:33 -0300

    now I understand rows and columns

--- widgets_alguma_coisa/lib/main.dart
+++ widgets_alguma_coisa/lib/main.dart
@@ -1,81 +1,110 @@
-// main.dart
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const Desafio());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class Desafio extends StatelessWidget {
+  const Desafio({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Atividade 1",
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.indigo,
-          leading: IconButton(
-            color: Colors.white,
-            icon: Icon(Icons.menu),
-            onPressed: () => {},
-          ),
-          title: Text("Page Title", style: TextStyle(color: Colors.white)),
-          actions: [
-            IconButton(
-              color: Colors.white,
-              onPressed: () => {},
-              icon: Icon(Icons.favorite),
-            ),
-            IconButton(
-              color: Colors.white,
-              onPressed: () => {},
-              icon: Icon(Icons.search),
-            ),
-            IconButton(
-              color: Colors.white,
-              onPressed: () => {},
-              icon: Icon(Icons.more_vert),
-            ),
-          ],
-        ),
-        body: Center(
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              children: [
-                SizedBox(
-                  height: 200,
-                  width: 300,
-                  child: Center(
-                    child: Text("DS - 2025", style: TextStyle(fontSize: 20)),
-                  ),
+      title: "Bolo",
+      home: HomeColunas(),
+    );
+  }
+}
+
+class HomeColunas extends StatelessWidget {
+  const HomeColunas({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Container(
+                color: const Color.fromARGB(255, 236, 230, 230),
+                child: Text(
+                  "Strawberry Lavnova",
+                  style: TextStyle(fontSize: 50),
                 ),
-                Image.network(
-                  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRvBRp3rDa9EMleXo2x1uRINZQdAYv-IfQBdA&s, width: 50.0,',
+              ),
+              SizedBox(height: 20),
+              Container(
+                width: 450,
+                color: const Color.fromARGB(255, 236, 230, 230),
+                child: Text(
+                  "Pavlova is a merinque based dessert named after the Russian bailerine Anna Pavlova. Pavlova features crisp crust and soft, light inside, topped with fruit and whipped cream.",
+                  style: TextStyle(fontSize: 20),
                 ),
-              ],
-            ),
+              ),
+              SizedBox(height: 20),
+              Container(
+                width: 450,
+                color: const Color.fromARGB(255, 236, 230, 230),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Row(
+                      children: [
+                        Icon(Icons.star, color: Colors.red, size: 30),
+                        Icon(Icons.star, color: Colors.red, size: 30),
+                        Icon(Icons.star, color: Colors.red, size: 30),
+                        Icon(Icons.star, color: Colors.red, size: 30),
+                        Icon(Icons.star, color: Colors.red, size: 30),
+                      ],
+                    ),
+                    Text("Muito bom"),
+                  ],
+                ),
+              ),
+              SizedBox(height: 20),
+              Container(
+                width: 450,
+                color: const Color.fromARGB(255, 236, 230, 230),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Column(
+                      children: [
+                        Icon(Icons.list_outlined, color: Colors.red, size: 30),
+                        Text("Lista"),
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        Icon(
+                          Icons.catching_pokemon,
+                          color: Colors.red,
+                          size: 30,
+                        ),
+                        Text("Pokemon"),
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        Icon(Icons.do_disturb, color: Colors.red, size: 30),
+                        Text("disturb"),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ],
           ),
-        ),
-        bottomNavigationBar: BottomNavigationBar(
-          type: BottomNavigationBarType.fixed,
-          backgroundColor: Colors.indigo,
-          selectedItemColor: Colors.white,
-          items: [
-            BottomNavigationBarItem(icon: Icon(Icons.home), label: "Início"),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.search),
-              label: "Pesquisa",
-            ),
-            BottomNavigationBarItem(icon: Icon(Icons.person), label: "Pessoa"),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.settings),
-              label: "Configurações",
-            ),
-          ],
-        ),
+          Image.network(
+            width: 400,
+            "https://cdn.atarde.com.br/img/Artigo-Destaque/1310000/1200x675/Davi-Brito-reflete-sobre-fama-e-perda-de-seguidore0131060700202503140933-9.webp?fallback=https%3A%2F%2Fcdn.atarde.com.br%2Fimg%2FArtigo-Destaque%2F1310000%2FDavi-Brito-reflete-sobre-fama-e-perda-de-seguidore0131060700202503140933.jpg%3Fxid%3D6624270&xid=6624270",
+          ),
+        ],
       ),
     );
   }
