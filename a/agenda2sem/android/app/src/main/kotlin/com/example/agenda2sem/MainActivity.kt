e26da85b70f5badc66e731fae40fdcf6e416980b
DadiAugusto <davidinizdalevedo9@gmail.com>
2025-08-08 09:36:38 -0300

    agenda com listview

---
+++ a/agenda2sem/android/app/src/main/kotlin/com/example/agenda2sem/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.agenda2sem
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ a/agenda2sem/lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:agenda2sem/myhomepage.dart';
+import 'package:flutter/material.dart';
+
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Tarefas",
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: Myhomepage()
+    );
+  }
+}
+
