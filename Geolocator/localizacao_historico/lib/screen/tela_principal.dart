99a19006b40cfc4463394bd6dbe5be5ed76f482c
DadiAugusto <davidinizdalevedo9@gmail.com>
2025-10-10 09:25:27 -0300

    make some fixes

--- Geolocator/localizacao_historico/lib/screen/tela_principal.dart
+++ Geolocator/localizacao_historico/lib/screen/tela_principal.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import '../database/db.dart'
-import '../localizacao/geo.dart'
 
 class TelaPrincipal extends StatefulWidget {
   const TelaPrincipal({super.key});
@@ -11,9 +9,37 @@ class TelaPrincipal extends StatefulWidget {
 
 class _TelaPrincipalState extends State<TelaPrincipal> {
   List<Map<String, dynamic>> _localizacoes = [];
-  
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Imagens no Centro'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const [
+            Image(
+              image: NetworkImage('https://i.redd.it/ety59mbye2o61.png'),
+              width: 100,
+              height: 100,
+            ),
+            SizedBox(height: 20),
+            Image(
+              image: NetworkImage('https://i.pinimg.com/736x/15/da/c1/15dac1e4bb13cac6ada41c9df64b5069.jpg'),
+              width: 100,
+              height: 100,
+            ),
+            SizedBox(height: 20),
+            Image(
+              image: NetworkImage('https://i.redd.it/ety59mbye2o61.png'),
+              width: 100,
+              height: 100,
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
--- PetShop/lib/main.dart
+++ PetShop/lib/main.dart
@@ -1,5 +1,5 @@
-import 'package:petshop/homepage.dart';
 import 'package:flutter/material.dart';
+import 'package:petshop/homepage.dart';
 
 
 void main() {
